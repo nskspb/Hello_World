24cbf5c6a37748c390bb7baae480e2bb76facca6
Ruslan <nugaev.ruslan2010@yandex.ru>
2023-01-14 21:08:34 +0300

    first commit

---
+++ homework/build/CMakeFiles/hello_world.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubunter/Desktop/study/C_plus_plus/OTUS/Hello_World/homework/example.cpp" "/home/ubunter/Desktop/study/C_plus_plus/OTUS/Hello_World/homework/build/CMakeFiles/hello_world.dir/example.cpp.o"
+  "/home/ubunter/Desktop/study/C_plus_plus/OTUS/Hello_World/homework/hello_world.cpp" "/home/ubunter/Desktop/study/C_plus_plus/OTUS/Hello_World/homework/build/CMakeFiles/hello_world.dir/hello_world.cpp.o"
+  "/home/ubunter/Desktop/study/C_plus_plus/OTUS/Hello_World/homework/move_me_out.cpp" "/home/ubunter/Desktop/study/C_plus_plus/OTUS/Hello_World/homework/build/CMakeFiles/hello_world.dir/move_me_out.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
